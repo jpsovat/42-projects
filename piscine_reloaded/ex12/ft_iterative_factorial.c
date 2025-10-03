763bf461d08a9ad43cdbbbf01d75d8441c710271
SEU NOME <seu-email@exemplo.com>
2025-10-03 09:40:41 +0100

    Add Piscine Reloaded - ex12 and ex13

---
+++ piscine_reloaded/ex12/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 08:43:49 by jsovat-d          #+#    #+#             */
+/*   Updated: 2025/10/03 09:33:49 by jsovat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	result = 1;
+	while (nb > 1)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
---
+++ piscine_reloaded/ex13/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 08:43:49 by jsovat-d          #+#    #+#             */
+/*   Updated: 2025/10/03 09:38:02 by jsovat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		result = 1;
+	else
+		result = nb * ft_recursive_factorial(nb - 1);
+	return (result);
+}
