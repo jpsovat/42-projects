74f16601dc91ec44520d203bef5739e1401e1590
SEU NOME <seu-email@exemplo.com>
2025-10-22 09:33:28 +0100

    Add libft update - ft_strncmp.c

---
+++ commom_core/Libft/ft_strncmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 09:17:04 by jsovat-d          #+#    #+#             */
+/*   Updated: 2025/10/22 09:33:04 by jsovat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
