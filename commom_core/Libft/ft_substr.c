91ca476dd64d921f5f379c5c76a5e81de155e9eb
SEU NOME <seu-email@exemplo.com>
2025-10-27 15:13:24 +0000

    Add libft update - ft_substr.c

---
+++ commom_core/Libft/ft_substr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 14:32:05 by jsovat-d          #+#    #+#             */
+/*   Updated: 2025/10/27 15:01:03 by jsovat-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	s_len;
+
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+    	substr = malloc(1);
+    	if (!substr)
+        	return (NULL);
+    	substr[0] = '\0';
+    	return (substr);
+	}
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = (malloc(sizeof(char) * (len + 1)));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
