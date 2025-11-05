7a3a3eccfbc5a991cc133d401bdf0740f8dd3e7f
SEU NOME <seu-email@exemplo.com>
2025-11-05 09:06:45 +0000

    Add ft_printf update - norminette and final adjustments

--- commom_core/ft_printf/ft_printf.c
+++ commom_core/ft_printf/ft_printf.c
@@ -6,11 +6,21 @@
 /*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 09:14:07 by jsovat-d          #+#    #+#             */
-/*   Updated: 2025/11/05 08:26:06 by jsovat-d         ###   ########.fr       */
+/*   Updated: 2025/11/05 09:06:19 by jsovat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_process_format(const char *format, va_list args, int *i)
+{
+	int	ret;
+
+	ret = ft_format_handler(format[*i], args);
+	if (ret == -1)
+		return (-1);
+	return (ret);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -20,25 +30,20 @@ int	ft_printf(const char *format, ...)
 	int		ret;
 
 	va_start(args, format);
-	i = 0;
+	i = -1;
 	count = 0;
-	ret = 0;
-	while (format[i])
+	while (format[++i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			ret += ft_format_handler(format[i], args);
+			ret = ft_process_format(format, args, &i);
 			if (ret == -1)
-				return (-1);
+				return (va_end(args), -1);
 			count += ret;
 		}
 		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
-		i++;
+			count += write(1, &format[i], 1);
 	}
 	va_end(args);
 	return (count);
