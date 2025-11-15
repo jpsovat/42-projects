047704f244b2a34625bd2d1253e5c5dc740c2403
jpsovat <jpsovat@gmail.com>
2025-11-15 15:42:07 +0000

    get_next_line | v.03

--- commom_core/get_next_line/get_next_line.c
+++ commom_core/get_next_line/get_next_line.c
@@ -5,127 +5,160 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsovat-d <jsovat-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/15 10:27:41 by jsovat-d          #+#    #+#             */
-/*   Updated: 2025/11/15 12:10:47 by jsovat-d         ###   ########.fr       */
+/*   Created: 2025/11/15 12:34:03 by jsovat-d          #+#    #+#             */
+/*   Updated: 2025/11/15 15:41:52 by jsovat-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
 
-// Duplicates a substring of s from index "start" up to "len" chars
-char *substr(const char *s, int start, int len)
+// -----------------------------------------
+// strlen (needed by join & extract_line)
+// -----------------------------------------
+static size_t   ft_strlen(const char *s)
 {
-    char	*out;
-	
-	out = malloc(len + 1);
-    if (!out)
-        return NULL;
-
-    for (int i = 0; i < len; i++)
-        out[i] = s[start + i];
-
-    out[len] = '\0';
-    return out;
+    size_t i = 0;
+    while (s && s[i])
+        i++;
+    return i;
 }
 
-
-char	*str_join(char *s1, char *s2)
+// -----------------------------------------
+// Finds position of '\n'. Returns index or -1
+// -----------------------------------------
+static int find_newline(const char *s)
 {
-    size_t	len1 = s1 ? strlen(s1) : 0;
-    size_t	len2 = strlen(s2);
-    char	*new = malloc(len1 + len2 + 1);
+    int i = 0;
+    while (s && s[i])
+    {
+        if (s[i] == '\n')
+            return i;
+        i++;
+    }
+    return -1;
+}
 
+// -----------------------------------------
+// Joins s1 + s2 into new string
+// s1 may be NULL
+// -----------------------------------------
+static char *str_join(char *s1, char *s2)
+{
+    size_t	len1;
+    size_t	len2;
+    char	*new;
+    size_t	i;
+	size_t	j;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new = malloc(len1 + len2 + 1);
     if (!new)
         return NULL;
-
-    if (s1)
-        strcpy(new, s1);
-    else
-        new[0] = '\0';
-
-    strcpy(new + len1, s2);
+	i = 0;
+    while (i < len1)
+    {
+        new[i] = s1[i];
+        i++;
+    }
+	j = 0;
+    while (j < len2)
+    {
+        new[i + j] = s2[j];
+        j++;
+    }
+    new[len1 + len2] = '\0';
 
     return new;
 }
 
-int	find_newline(char *buffer)
+// -----------------------------------------
+// Create substring
+// -----------------------------------------
+static char *substr(const char *s, int start, int len)
 {
-	int	i;
+    char *out;
+    int  i;
 
+	out = malloc(len + 1);
+    if (!out)
+        return NULL;
 	i = 0;
-	while (buffer[i])
-	{
-		if (buffer[i] == '\n')
-			return (i);
-		i++;
-	}
-	return (-1);	// no newline found
+    while (i < len)
+    {
+        out[i] = s[start + i];
+        i++;
+    }
+    out[len] = '\0';
+    return out;
 }
 
-int	main(void)
+// -----------------------------------------
+// Extract line + adjust stash
+// -----------------------------------------
+static char *extract_line(char **stash_ptr)
 {
-	int			fd;
-	ssize_t		bytes_read;
-	char		buffer[42];
-	int			newline;
-	char		*stash;
-	char		*temp;
-	int			nl;
-	char		*line;
-	char		*remainder;
+    char	*stash;
+	char	*stash_ptr;
+    int		nl;
+    char	*line;
+    char	*remainder;
 
-	// getting the "id" of the file
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		printf("Cannot open file\n");
-		return (1);
-	}
+	nl = find_newline(stash);
+    if (nl >= 0)
+    {
+        line = substr(stash, 0, nl);
+        remainder = substr(stash, nl + 1, ft_strlen(stash) - (nl + 1));
+        free(stash);
+        *stash_ptr = remainder;
+        return line;
+    }
+    else
+    {
+        line = substr(stash, 0, ft_strlen(stash));
+        free(stash);
+        *stash_ptr = NULL;
+        return line;
+    }
+}
 
-	stash = NULL;	// the big string we grow with chunks
+// -----------------------------------------
+// get_next_line
+// -----------------------------------------
+int get_next_line(int fd, char **line)
+{
+    static char *stash = NULL;
+    char        buffer[42];
+    ssize_t     bytes_read;
+    char       *temp;
 
-	//	loop to create chunks into buffer and add it to stash
-	while ((bytes_read = read(fd, buffer, 4)) > 0)
-	{
-		
-		buffer[bytes_read] = '\0';
+    if (fd < 0 || !line)
+        return (-1);
 
-		// Add new chunk to stash
+    while ((bytes_read = read(fd, buffer, 5)) > 0)
+    {
+        buffer[bytes_read] = '\0';
+
         temp = str_join(stash, buffer);
         free(stash);
         stash = temp;
+        if (!stash)
+            return (-1);
 
-		// repeat the loop adding chunks untill we find a new line
-		if (find_newline(buffer) != -1)
-			break;
-	}
+        if (find_newline(stash) != -1)
+            break;
+    }
 
-	printf("Full line stored in stash: \"%s\"\n", stash);
+    if (bytes_read < 0)
+        return (-1);
 
-	// find the \n postition
-	nl = find_newline(stash);
+    if (!stash || stash[0] == '\0')
+        return (0);
 
-	if (nl >= 0)
-	{
-		//everything before \n
-		line = substr(stash, 0, nl);
+    *line = extract_line(&stash);
+    if (!*line)
+        return (-1);
 
-		//everything after \n
-		remainder = substr(stash, nl + 1, strlen(stash) - (nl + 1));
-
-		printf("LINE: \"%s\"\n", line);
-    	printf("REMAINDER: \"%s\"\n", remainder);
-
-    	free(line);
-    	free(remainder);
-	}
-	
-
-	free(stash);
-	
-	return (0);
+    return (1);
 }
